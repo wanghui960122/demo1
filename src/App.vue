42114ddee9ddd4c26921a0f3f0a381e836e9c3c4
wanghui960122 <1205058003@qq.com>
2020-05-11 10:37:44 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="http://m.imeitou.com/uploads/allimg/2020010412/q3spfwsnoyg.jpeg">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h2>迈出历史性的一步</h2>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
